956aaebd7f6de838aa43f94e78a40eb2bb5a2bbc
skykeeper0 <trungdinh0429@gmail.com>
2017-05-14 17:05:00 -0700

    add clear table to test

--- main_test.go
+++ main_test.go
@@ -5,9 +5,11 @@ import (
 			"testing"
 
 			"."
+	"log"
 )
 
 var a main.App
+
 
 func TestMain(m *testing.M) {
 	a = main.App{}
@@ -22,4 +24,23 @@ func TestMain(m *testing.M) {
 	code:= m.Run()
 	clearTable()
 	os.Exit(code)
+
+	const tableCreateQuery = `CREATE TABLE IF NOT EXIST products
+	(
+		id SERIAL,
+		name TEXT NOT NULL,
+		price NUMERIC(10,2) NOT NULL DEFAUTL 0.00,
+		CONSTRAINT products_pkey PRIMARY KEY (id)
+	)`
+	
+	func ensureTableExists() {
+		if _, err:= a.DB.Exec(tableCreationQuery); err != nil{
+			log.Fatal(err)
+		}
+	}
+
+	func clearTable() {
+		a.DB.Exec("DELETE FROM products")
+		a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+	}
 }
